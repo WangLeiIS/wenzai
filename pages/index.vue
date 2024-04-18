d1a179de89213033d310532b1dd2030a5dbe44e4
wang jiaming <46948956+WangLeiIS@users.noreply.github.com>
2024-04-18 11:35:40 +0800

    更新 index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -57,7 +57,7 @@
     prompt.value.question = inputMessage.value.slice(startPosition.value+2, cursorPosition.value-2)
     prompt.value.before_question = inputMessage.value.slice(orginStartPosition.value, startPosition.value)
     const err_answer = ":(我也不知道"
-    const answer = await fetch(`/api/chat`, {
+    const answer = await useFetch(`/api/chat`, {
       body: JSON.stringify(prompt.value),
       method: 'post'
     });
